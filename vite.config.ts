a3aed4eae52c701c6f21dcccd184c00383368101
Together <togetheryear@gmail.com>
2023-05-19 23:52:23 +0800

    SFC

--- vite.config.ts
+++ vite.config.ts
@@ -4,33 +4,37 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [vue({ script: { reactivityTransform: true } })],
-  resolve: {
-    alias: {
-      '@': path.resolve('src')
-    }
-  },
-  build: {
-    sourcemap: false,
-    target: 'esnext'
-  },
-  optimizeDeps: {
-    include: ['axios']
-  },
-  base: './',
-  envDir: './env',
-  server: {
-    host: '0.0.0.0',
-    port: 6768,
-    open: true,
+    plugins: [
+        vue({
+            script: {}
+        })
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve('src')
+        }
+    },
+    build: {
+        sourcemap: false,
+        target: 'esnext'
+    },
+    optimizeDeps: {
+        include: ['axios']
+    },
     base: './',
-    strictPort: true
-    // proxy: {
-    //   '^/api': {
-    //     target: 'http://127.0.0.1',
-    //     changeOrigin: true,
-    //     rewrite: (t) => t.replace(/^\/api/, '')
-    //   }
-    // }
-  }
+    envDir: './env',
+    server: {
+        host: '0.0.0.0',
+        port: 6768,
+        open: true,
+        base: './',
+        strictPort: true
+        // proxy: {
+        //   '^/api': {
+        //     target: 'http://127.0.0.1',
+        //     changeOrigin: true,
+        //     rewrite: (t) => t.replace(/^\/api/, '')
+        //   }
+        // }
+    }
 })
