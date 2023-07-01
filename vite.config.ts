b260dcd51b164723ece611821340a3062a322f71
Together <togetheryear@gmail.com>
2023-07-01 12:10:41 +0800

    defineModel

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,9 @@ import path from 'path'
 export default defineConfig({
     plugins: [
         vue({
-            script: {}
+            script: {
+                defineModel: true
+            }
         })
     ],
     resolve: {
