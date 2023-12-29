eaba85559a93f5399c25679c44349532dc9187b0
zhangwenjun <1346502061@qq.com>
2023-12-29 17:00:27 +0800

    Update:Type

--- src/libs/AActor.ts
+++ src/libs/AActor.ts
@@ -3,7 +3,7 @@ import { EventSystem } from "./EventSystem";
 abstract class AActor extends EventSystem {
     constructor() { super() }
 
-    public abstract InitStates(): Record<string, any>
+    public abstract InitStates(): Record<string, unknown>
 
     public abstract InitHooks(): void
 
