a01dd32e327b54998c11355c127acf49bc175c4f
TSingleton <togetheryear@gmail.com>
2024-07-24 16:36:27 +0800

    Update:缓存装饰器

--- Src/Decorators/DR.ts
+++ Src/Decorators/DR.ts
@@ -1,4 +1,5 @@
-import { onMounted, onUnmounted } from "vue"
+import { isRef, onMounted, onUnmounted } from "vue"
+import { useRoute } from "vue-router"
 
 namespace DR {
     export function ClassDec() {
@@ -36,6 +37,8 @@ namespace DR {
     const debounceMap = new Map<string, NodeJS.Timeout>()
 
     const throttleMap = new Map<string, number>()
+
+    const cacheMap = new Map<string, Array<{ key: string, value: unknown }>>()
 
     /**
      * 防抖 默认 500 毫秒
@@ -85,6 +88,90 @@ namespace DR {
                     original(...args)
                 }
                 throttleMap.set(key, lastTime)
+            }
+        }
+    }
+
+    /**
+     * 缓存页面 参数为字符串 如果只是普通的 只要 '属性名' 或者 '对象名' 即可 如果是对象的属性 则 '对象.属性名' (目前不支持嵌套)
+     */
+    export function Cache(...needs: Array<string>) {
+        return function <T extends new (...args: Array<any>) => Object>(C: T) {
+            return class extends C {
+                constructor(...args: Array<any>) {
+                    super(...args)
+                    this.needCache = needs
+                    this.Hooks()
+                }
+
+                private currentUrl = ''
+
+                private needCache: Array<string> = []
+
+                private Hooks() {
+                    this.Get()
+                    onUnmounted(() => {
+                        this.Set()
+                    })
+                }
+
+                private Get() {
+                    const route = useRoute()
+                    this.currentUrl = route.path
+                    const current = cacheMap.get(this.currentUrl)
+                    if (current) {
+                        for (let c of current) {
+                            const es = `${c.key} = ${typeof c.value == 'string' ? `'${c.value}'` : c.value}`
+                            eval(es)
+                        }
+                    }
+                }
+
+                private Set() {
+                    const cache: Array<{ key: string, value: unknown }> = []
+                    for (let c of this.needCache) {
+                        const deep = c.split('.')
+                        if (deep.length == 1) {
+                            let es = `this['${deep}']`
+                            if (typeof eval(es) == 'object') {
+                                if (isRef(eval(es))) {
+                                    es += '.value'
+                                    cache.push({
+                                        key: es,
+                                        value: eval(es)
+                                    })
+                                }
+                                else {
+                                    if (eval(es) != null) {
+                                        const keys = Object.keys(eval(es))
+                                        for (let k of keys) {
+                                            const c = `${es}['${k}']`
+                                            cache.push({
+                                                key: c,
+                                                value: eval(c)
+                                            })
+                                        }
+                                    }
+                                }
+                            }
+                            else {
+                                cache.push({
+                                    key: es,
+                                    value: eval(es)
+                                })
+                            }
+                        }
+                        else {
+                            let es = 'this'
+                            deep.forEach(d => es += `['${d}']`)
+                            cache.push({
+                                key: es,
+                                value: eval(es)
+                            })
+                        }
+                    }
+                    cacheMap.set(this.currentUrl, cache)
+                }
             }
         }
     }
