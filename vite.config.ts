426d853345eab4066ef2def0aa613aab542e96aa
TSingleton <togetheryear@gmail.com>
2024-07-26 14:43:39 +0800

    Update:Format

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
 import path from 'path';
 
 export default defineConfig({
@@ -9,8 +8,7 @@ export default defineConfig({
             script: {
                 defineModel: true
             }
-        }),
-        vueJsx()
+        })
     ],
     resolve: {
         alias: {
