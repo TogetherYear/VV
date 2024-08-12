aab53aaf4f914c379164542a70be434d48abf886
TSingleton <togetheryear@gmail.com>
2024-08-12 15:05:15 +0800

    Update:Exec

--- Shell/Compress.js
+++ Shell/Compress.js
@@ -1,6 +1,7 @@
 const compressing = require('compressing');
 const path = require('path');
 const fs = require('fs');
+const cp = require('child_process');
 
 const inputPath = path.join(__dirname, '../Dist');
 const outputPath = path.join(__dirname, '../Dist.zip');
@@ -11,4 +12,8 @@ zipStream.addEntry(inputPath);
 
 const destStream = fs.createWriteStream(outputPath);
 
-zipStream.pipe(destStream)
+const result = zipStream.pipe(destStream)
+
+result.on('finish', () => {
+    cp.exec(`explorer.exe /select,"${outputPath}"`);
+})
