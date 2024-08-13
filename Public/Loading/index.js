b835216ccb8a3109e096e5b7e30680b3e9146614
TSingleton <togetheryear@gmail.com>
2024-08-13 15:07:29 +0800

    Update:Format

--- Public/Loading/index.js
+++ Public/Loading/index.js
@@ -1,14 +1,14 @@
-const loading = document.querySelector('#Loading')
-const app = document.querySelector('#App')
+const loading = document.querySelector('#Loading');
+const app = document.querySelector('#App');
 
 window.ShowLoading = () => {
-    app.style.opacity = '0'
-    loading.style.opacity = '1'
-    loading.style.pointerEvents = 'all'
-}
+    app.style.opacity = '0';
+    loading.style.opacity = '1';
+    loading.style.pointerEvents = 'all';
+};
 
 window.HideLoading = () => {
-    app.style.opacity = '1'
-    loading.style.opacity = '0'
-    loading.style.pointerEvents = 'none'
-}
+    app.style.opacity = '1';
+    loading.style.opacity = '0';
+    loading.style.pointerEvents = 'none';
+};
