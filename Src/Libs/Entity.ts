707967572bc054e86b7ecbd01660121f6bd59075
章文俊 <1346502061@qq.com>
2025-01-15 10:42:20 +0800

    Update:Component

--- Src/Libs/Entity.ts
+++ Src/Libs/Entity.ts
@@ -2,6 +2,8 @@ import { Time } from '@/Utils/Time';
 import { EventSystem } from './EventSystem';
 import { TEntity } from '@/Decorators/TEntity';
 import { TRouter } from '@/Decorators/TRouter';
+import { TComponent } from '@/Decorators/TComponent';
+import { Component } from './Component';
 
 /**
  * 根 我用来代理一些变量的
@@ -25,6 +27,25 @@ class Entity extends EventSystem {
      */
     public get Query() {
         return TRouter.currentQuery;
+    }
+
+    /**
+     * 获取当前页面所有存活的 Component
+     */
+    public GetAllComponent() {
+        return TComponent.components;
+    }
+
+    /**
+     * 根据条件获取 Component
+     */
+    public GetComponent<T extends Component>(Condition: (instance: T) => boolean): T | null {
+        for (let c of TComponent.components) {
+            if (Condition(c as T)) {
+                return c as T;
+            }
+        }
+        return null;
     }
 }
 
